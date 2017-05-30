07984de711904f1519eed239c5c5b17e08c09ad5
Saad El Hajjaji <saadel@users.noreply.github.com>
2017-05-30 10:36:38 +0200

    Fix Proptypes (#36)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MenuItem from './item';
 import MenuButton from './button';
 
